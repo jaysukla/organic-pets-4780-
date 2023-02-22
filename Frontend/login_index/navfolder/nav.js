0679aba0efa263fb3cf8a9e51662e4d6492c880a
unknown <mohimabahadur@gmail.com>
2023-02-23 02:19:30 +0530

    log in index home page

---
+++ Frontend/login_index/navfolder/nav.js
@@ -0,0 +1,23 @@
+window.onscroll = function() {myFunction()}
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+}
+
+// const resources = document.getElementById("resources")
+// const resourcesMenu = document.getElementById("resourcesMenu")
+
+// resources.addEventListener('mousemove', ()=>{
+//     resourcesMenu.style.display="flex"
+// })
+
+// resources.addEventListener('mouseout', ()=>{
+//     resourcesMenu.style.display="none"
+// })
