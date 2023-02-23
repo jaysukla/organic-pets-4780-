5ba29cf273cf1b623666994e003c6198e5370dda
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2023-02-24 00:19:36 +0530

    footerdone

--- Frontend/JS/Navbar.js
+++ Frontend/JS/Navbar.js
@@ -8,14 +8,20 @@ window.onscroll = function () {
   }
 };
 let navclickbtn = document.getElementById("navclickbtn");
-
+navclickbtn.innerHTML = `<svg data-navclose xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 96 960 960" width="30"><path data-navclose d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/></svg>`;
 navclickbtn.addEventListener("click", () => {
   let NavlinkParent = document.getElementById("NavlinkParent");
-  NavlinkParent.classList.toggle("navtranslate");
+  let switcher = NavlinkParent.classList.toggle("navtranslate");
+  if (switcher) {
+    navclickbtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 96 960 960" width="30"><path d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/></svg>`;
+  } else {
+    navclickbtn.innerHTML = `<svg data-navclose xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 96 960 960" width="30"><path data-navclose d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/></svg>`;
+  }
 });
 
 document.addEventListener("click", (e) => {
   if (!e.target.matches("[data-navclose]")) {
     NavlinkParent.classList.remove("navtranslate");
+    navclickbtn.innerHTML = `<svg data-navclose xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 96 960 960" width="30"><path data-navclose d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/></svg>`;
   }
 });
