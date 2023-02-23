0c210253d97422911716404ba3b6d8698d4e7269
Sitansu Mandal <85841972+sitansu04@users.noreply.github.com>
2023-02-24 00:49:11 +0530

    updated

--- evaluation/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-const connection = mongoose.connect("mongodb+srv://sitansu:smandal@cluster0.nhfpm6o.mongodb.net/evalnxt?retryWrites=true&w=majority");
-module.exports = {
-  connection
-};
