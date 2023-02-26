8c81d275a07b398adf37a5af0314d2ee5a647bad
unknown <mohimabahadur@gmail.com>
2023-02-26 15:19:51 +0530

    delet fun

--- Frontend/login_index/loginnav.js
+++ Frontend/login_index/loginnav.js
@@ -50,29 +50,38 @@ fetchdata()
 function renderData(data){
 
   event_card.innerHTML=""
+  if(!data){
+    document.querySelector('#content>h2').innerText="You don't have any event types yet."
+    document.querySelector('#content>p').innerText="You'll want to add an event type to allow people to schedule with you."
+    return;
+  }
+
+  document.querySelector('#content>h2').innerText="";
+  document.querySelector('#content>p').innerText="";
+
   event_card.innerHTML=`${data.map((el)=>{
-    let s = el.starttime.split(":")
-    let e = el.endtime.split(":")
+    let s =el.starttime.split(":")
+    let e =el.endtime.split(":")
 
-    let shour = s[0]
-    let smint= s[1].split(" ")[0]
-    let stime = +(shour+"."+smint)
+    let shour =s[0]
+    let smint=s[1].split(" ")[0]
+    let stime =+(shour+"."+smint)
 
-    let ehour = e[0]
-    let emint= e[1].split(" ")[0]
-    let etime = +(ehour+"."+emint)
-    let diff = (Math.abs(etime-stime)+"").split(".")
-    let mint ;
+    let ehour =e[0]
+    let emint=e[1].split(" ")[0]
+    let etime =+(ehour+"."+emint)
+    let diff =(Math.abs(etime-stime)+"").split(".")
+    let mint;
     if(diff[0]==0){
-      mint = diff[1]+"mint"
+      mint =diff[1]+"mint"
     }else{
-      mint = diff[0]+hr+" "+diff[1]+mint
+      mint =diff[0]+'hr'+" "+diff[1]+'mint'
     }
         return `
         <div id="event_card">
                 <div>
                     <p>${el.title}</p>
-                    <img src="https://cdn-icons-png.flaticon.com/512/126/126472.png" alt="">
+                    <img class="deletebtn" name=${el._id} src="https://cdn-icons-png.flaticon.com/512/1345/1345874.png" alt="">
                 </div>
 
                 <div>
@@ -84,24 +93,35 @@ function renderData(data){
             </div> `
     }).join("")}
   `
-}
 
-                                                                            
+  const deletebtn = document.querySelectorAll(".deletebtn")
+// console.log(deletebtn)
+  for(let i=0;i<deletebtn.length;i++){
+    deletebtn[i].addEventListener("click",(e)=>{
+      // deletefun(e['target']['name'])
+    })
+  } 
 
-async function deletefun(id){
-  try{
-      let res = await fetch(`https://long-tan-fossa-belt.cyclic.app/delete/${id}`,{
-          headers:{
-              "Content-Type": "application/json",
-              Authorization:access_token.token
-          },
-          method:"DELETE"
-      })
-      if(res.ok){
-        alert("successfully delete event")
-      }
-  }
-  catch(err){
-      console.log(err)
-  }
 }
+                                                                   
+
+// async function deletefun(id){
+//   let obj={email:'',id}
+//   console.log(obj)
+//   try{
+//       let res = await fetch(`https://impossible-pear-waistcoat.cyclic.app/delete/`,{
+//           headers:{
+//               "Content-Type": "application/json",
+//           },
+//           method:"DELETE",
+//           body: JSON.stringify(obj)
+//       })
+//       if(res.ok){
+//         renderData(data)
+//         alert("successfully delete event")
+//       }
+//   }
+//   catch(err){
+//       console.log(err)
+//   }
+// }
